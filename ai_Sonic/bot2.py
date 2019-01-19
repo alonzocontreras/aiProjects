b672de1af7210516f55ed45fdad7cbf0c6f81bf5
alonzo <alonzocontrera14@gmail.com>
2019-01-18 20:34:46 -0800

    succesful runs for sonic,38 generations completed, more needed

--- ai_Sonic/bot2.py
+++ ai_Sonic/bot2.py
@@ -6,48 +6,77 @@ import pickle
 
 env = retro.make('SonicTheHedgehog-Genesis', 'GreenHillZone.Act1')
 imgarray = []
+xpos_end = 0
 
 def eval_genomes(genomes, config):
-   for genome_id, genome in genomes:
-   	    observ = env.reset()
-   	    action = env.action_space.sample()
+ for genome_id, genome in genomes:
+  observ = env.reset()
+  action = env.action_space.sample()
 
-   	    inx, iny, inc = env.observation_space.shape  #the x, y, and colors
-   	    inx = int(inx/8)
-   	    iny = int(iny/8)
+  inx, iny, inc = env.observation_space.shape  #the x, y, and colors
+  inx = int(inx/8)
+  iny = int(iny/8)
 
-   	    net = neat.nn.recurrent.RecurrentNetwork.create(genome, config)
+  net = neat.nn.recurrent.RecurrentNetwork.create(genome, config)
 
-   	    current_max_fitness = 0
-   	    fitness_current =  0
-   	    frame = 0
-   	    counter = 0
-   	    xpos = 0
-   	    xpos_max = 0
+  current_max_fitness = 0
+  fitness_current =  0
+  frame = 0
+  counter = 0
+  xpos = 0
+  xpos_max = 0
 
-   	    done = False
+  done = False
 
-   	    while not done:
-        	env.render()
-        	frame += 1
+  while not done:
+      env.render()
+      frame += 1
 
-        	observ = cv2.resize(observ, (inx, iny))
-        	observ = cv2.cvtColor(observ, cv2.COLOR_BGR2GrAY)
-        	observ = np.reshape(observ, (inx, iny))
+      observ = cv2.resize(observ, (inx, iny))
+      observ = cv2.cvtColor(observ, cv2.COLOR_BGR2GRAY)
+      observ = np.reshape(observ, (inx,iny)) 
 
-        	for x in observ:
-        		for y in x:
-        			imgarray.append(y)
+      imgarray = np.ndarray.flatten(observ)
+      nnOutput = net.activate(imgarray)
+      observ, reward, done, info = env.step(nnOutput)
+      
+      xpos = info['x']
+      xpos_end = info['screen_x_end']
+      
+      if xpos > xpos_max:
+          fitness_current += 1
+          xpos_max = xpos
+      
+      if xpos == xpos_end and xpos > 500:
+          fitness_current += 100000
+          done = True
+      
+      fitness_current += reward
+      
+      if fitness_current > current_max_fitness:
+          current_max_fitness = fitness_current
+          counter = 0
+      else:
+          counter += 1
+          
+      if done or counter == 250:
+          done = True
+          print(genome_id, fitness_current)
+          
+      genome.fitness = fitness_current
 
-        	nnOutput = net.activate(imgarray)
-
-        	print(nnOutput)
-
-
-config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, 
-	                 neat.DefaultSpeciesSet, neat.DefaultStagnation, 
-	                 'config-feedforward')
+config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                     neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                     'config-feedforward')
 
 population = neat.Population(config)
 
+population.add_reporter(neat.StdOutReporter(True))
+stats = neat.StatisticsReporter()
+population.add_reporter(stats)
+population.add_reporter(neat.Checkpointer(10))
+
 winner = population.run(eval_genomes)
+
+with open('winner.pkl', 'wb') as output:
+    pickle.dump(winner, output, 1)
